56ab4a5c9b47421bca50308cff92fd0fb76c5890
deenamohdous <dina.mohdous@gmail.com>
2022-07-04 22:53:10 +0300

    navbar

--- task1/src/component/NavBar/index.js
+++ task1/src/component/NavBar/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./style.css";
+import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav className="navbar navbar-dark bg-dark">
+<div className="container-fluid">
+    <a className="navbar-brand" >Movie App</a>
+    <div className='navBarButton'>
+
+ <button className="btn btn-outline-success" > < Link  className='links' to={"/"}>Home</Link></button>
+      <button className="btn btn-outline-success" > <Link className='links' to={"/addTOFav"}>Favorite</Link></button></div>
+
+  </div>
+</nav>
   )
 }
 
